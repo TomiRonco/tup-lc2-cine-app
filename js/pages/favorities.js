042767f6c2a78394df40e6f6965f9b03b1f58932
Tomas Roncoroni <roncoronit@gmail.com>
2023-07-04 10:51:48 -0300

    cine

--- js/pages/favorities.js
+++ js/pages/favorities.js
@@ -25,21 +25,31 @@ async function mostrarPeliculasFavoritas() {
                 const pelicula = await obtenerPeliculaPorCodigo(codigo);
 
                 if (pelicula) {
-                    const { poster_path, title, id, original_title, original_language, release_date, overview } = pelicula;
+                    const {
+                        poster_path,
+                        title,
+                        id,
+                        original_title,
+                        original_language,
+                        release_date,
+                        overview
+                    } = pelicula;
 
                     const tarjeta = document.createElement('div');
                     tarjeta.classList.add('favorita');
                     tarjeta.innerHTML = `
-                  <img class="poster" src="https://image.tmdb.org/t/p/w500/${poster_path}">
-                  <h3 class="titulo">${title}</h3>
-                  <p><b>Código:</b> <span class="codigo">${id}</span><br>
-                  <b>Título original:</b> ${original_title}<br>
-                  <b>Idioma original:</b> ${original_language}<br>
-                  <b>Año:</b> ${release_date}<br>
-                  <b>Resumen:</b> ${overview}<br>
-                  <button class="button radius medium eliminar-favorita">Eliminar de Favoritos</button>`;
+                <img class="poster" src="https://image.tmdb.org/t/p/w500/${poster_path}">
+                <h3 class="titulo">${title}</h3>
+                <p><b>Código:</b> <span class="codigo">${id}</span><br>
+                <b>Título original:</b> ${original_title}<br>
+                <b>Idioma original:</b> ${original_language}<br>
+                <b>Año:</b> ${release_date}<br>
+                <b>Resumen:</b> ${overview}<br>
+                <div class="video-container"></div>
+                <button class="button radius medium eliminar-favorita">Eliminar de Favoritos</button>`;
 
                     contenedorFavoritas.appendChild(tarjeta);
+                    await agregarVideoAPelicula(id, tarjeta.querySelector('.video-container'));
                 }
             }
 
@@ -56,8 +66,30 @@ async function mostrarPeliculasFavoritas() {
 // Función para obtener los detalles de una película por su código
 async function obtenerPeliculaPorCodigo(codigo) {
     const resultados = await obtenerDatosAPI();
-    const pelicula = resultados.find(pelicula => pelicula.id === codigo);
+    const pelicula = resultados.find((pelicula) => pelicula.id === codigo);
     return pelicula;
+}
+
+// Función para agregar video a la tarjeta de la película
+async function agregarVideoAPelicula(codigo, contenedorVideo) {
+    const apiKey = 'd2ec227d94b2f1aabdbc91b53a9fce0e';
+    const videoUrl = `https://api.themoviedb.org/3/movie/${codigo}/videos?api_key=${apiKey}`;
+
+    try {
+        const response = await fetch(videoUrl);
+        const data = await response.json();
+        const video = data.results[0];
+
+        if (video) {
+            const iframe = document.createElement('iframe');
+            iframe.src = `https://www.youtube.com/embed/${video.key}`;
+            iframe.setAttribute('width', '300');
+            iframe.setAttribute('height', '200');
+            contenedorVideo.appendChild(iframe);
+        }
+    } catch (error) {
+        console.error('Error al obtener el video:', error);
+    }
 }
 
 // Función para eliminar una película de la lista de favoritos
@@ -74,7 +106,7 @@ function eliminarPeliculaFavorita(codigo) {
 
 // Agregar evento de clic a través de la delegación de eventos para eliminar películas desde el botón "Eliminar de Favoritos"
 const contenedorFavoritas = document.getElementById('contenedorFavoritas');
-contenedorFavoritas.addEventListener('click', event => {
+contenedorFavoritas.addEventListener('click', (event) => {
     if (event.target.classList.contains('eliminar-favorita')) {
         const tarjeta = event.target.closest('.favorita');
         const codigo = parseInt(tarjeta.querySelector('.codigo').textContent);
