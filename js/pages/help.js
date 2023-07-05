be0c6ae5bedaf5b7a11d52b9fbc3ebd2c3e31067
Tomas Roncoroni <roncoronit@gmail.com>
2023-07-05 10:17:02 -0300

    cine

--- js/pages/help.js
+++ js/pages/help.js
@@ -3,6 +3,8 @@
 })();
 
 const form = document.querySelector('.form');
+const mensajeEnviado = document.getElementById('mensaje-enviado');
+const mensajeError = document.getElementById('mensaje-error');
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -12,6 +14,13 @@ form.addEventListener('submit', function (event) {
     const email = document.getElementById('email').value;
     const message = document.getElementById('mensaje').value;
 
+    // Validar que los campos estén rellenados
+    if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+        mensajeError.textContent = 'Por favor, completa todos los campos.';
+        mensajeError.style.display = 'block';
+        return; // Detener la ejecución si hay campos vacíos
+    }
+
     // Enviar el correo electrónico
     emailjs.send('service_buib11c', 'template_0xpvxun', {
         from_name: name,
@@ -20,11 +29,29 @@ form.addEventListener('submit', function (event) {
     })
         .then(function () {
             // Éxito: el correo electrónico se envió correctamente
-            alert('El mensaje ha sido enviado correctamente.');
+            mensajeEnviado.textContent = 'El mensaje ha sido enviado correctamente.';
+            mensajeEnviado.style.display = 'block';
             form.reset();
+
+            // Ocultar el mensaje después de 2.5 segundos
+            setTimeout(function () {
+                mensajeEnviado.style.display = 'none';
+            }, 2500);
+
+            // Ocultar el mensaje de error si estaba visible
+            mensajeError.style.display = 'none';
         }, function (error) {
             // Error: el correo electrónico no se pudo enviar
+            mensajeError.textContent = 'Ocurrió un error al enviar el mensaje. Por favor, inténtalo nuevamente.';
+            mensajeError.style.display = 'block';
             console.error('Error al enviar el correo electrónico:', error);
-            alert('Ocurrió un error al enviar el mensaje. Por favor, inténtalo nuevamente.');
+
+            // Ocultar el mensaje después de 2.5 segundos
+            setTimeout(function () {
+                mensajeError.style.display = 'none';
+            }, 2500);
+
+            // Ocultar el mensaje de enviado si estaba visible
+            mensajeEnviado.style.display = 'none';
         });
 });
